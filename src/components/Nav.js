ebedded78af0530b58c123b8c96347066be64511
Bella Yang <bellalucajosef@gmail.com>
2021-11-13 12:05:10 +0100

    tested publishing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
 import React from "react";
+import logo from "../img/bento.png";
+import logo2 from "../img/bento-2.png";
+import logo3 from "../img/bento-3.png";
 
 function Nav() {
 	return (
 		<div className="nav">
-			<h1>BENTO</h1>
+			<h1>
+				<img src={logo3} id="logo" alt="logo" />
+			</h1>
 			<ul className="link-con">
 				<li>About Us</li>
 				<li>Our Concept</li>
--- src/data/menus.js
+++ src/data/menus.js
@@ -1,7 +1,5 @@
 export const menus = [
 	{ title: "Vegeterain Bento", detail: "this part is detail", img: "" },
-	{ title: "Vegan Bento", detail: "this part is detail", img: "" },
-	{ title: "Chicken Bento", detail: "this part is detail", img: "" },
-	{ title: "Beef Bento", detail: "this part is detail", img: "" },
+	{ title: "Meat Bento", detail: "this part is detail", img: "" },
 	{ title: "Random Bento", detail: "this part is detail", img: "" },
 ];

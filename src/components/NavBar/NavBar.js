7f3d840e44158d6e1c8f21f8f73fc89e071ec720
Bella Yang <bellalucajosef@gmail.com>
2021-11-15 09:54:56 +0100

    add functions for nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,40 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../img/Bento.png";
 import { Link } from "react-router-dom";
-import {
-	Navbar,
-	NavDropdown,
-	Container,
-	Nav,
-	FormControl,
-	Form,
-	Button,
-} from "react-bootstrap";
+import hamburger from "../../img/icons/hamburger.png";
 
 function NavBar() {
+	const [show, setShow] = useState(false);
+	function showDropNav() {
+		setShow(!show);
+	}
 	return (
-		<div className="nav">
-			<Link to="/">
-				<img src={logo} id="logo" alt="logo" />
-			</Link>
-			<ul className="link-con">
-				<Link to="/about">
-					<li>About Us</li>
+		<>
+			<div className="nav">
+				<Link to="/">
+					<img src={logo} id="logo" alt="logo" />
 				</Link>
-				<Link to="/services">
-					<li>Services</li>
-				</Link>
-				<Link to="/menu">
-					<li>Menu</li>
-				</Link>
-				<Link to="/location">
-					<li>Daily Location</li>
-				</Link>
-				<Link to="/contact">
-					<li>Contact Us</li>
-				</Link>
-			</ul>
-		</div>
+				<ul className="link-con">
+					<Link to="/about">
+						<li>About Us</li>
+					</Link>
+					<Link to="/services">
+						<li>Services</li>
+					</Link>
+					<Link to="/menu">
+						<li>Menu</li>
+					</Link>
+					<Link to="/location">
+						<li>Daily Location</li>
+					</Link>
+					<Link to="/contact">
+						<li>Contact Us</li>
+					</Link>
+				</ul>
+				<img onClick={showDropNav} src={hamburger} alt="menu" id="hamburger" />
+			</div>
+			<div>
+				<ul
+					style={!show ? { display: "none" } : { display: "block" }}
+					className="drop-con"
+				>
+					<Link to="/about">
+						<li>About Us</li>
+					</Link>
+					<Link to="/services">
+						<li>Services</li>
+					</Link>
+					<Link to="/menu">
+						<li>Menu</li>
+					</Link>
+					<Link to="/location">
+						<li>Daily Location</li>
+					</Link>
+					<Link to="/contact">
+						<li>Contact Us</li>
+					</Link>
+				</ul>
+			</div>
+		</>
 	);
 }
 
